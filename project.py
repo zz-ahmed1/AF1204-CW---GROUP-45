96185ceebb51107f0e1de42de553c4bf786ff78b
JJ-codingstuff <junaid.juyel@bayes.city.ac.uk>
2026-03-05 10:53:35 +0000

    Add comments and import libraries in project.py
    Added comments and import statements for libraries.

--- project.py
+++ project.py
@@ -1,1 +1,6 @@
+#commenting as i go along so it can go into the report :p
+#importing relevant libraries
+import marimo as mo      #our notebook
+import pandas as pd      #used for data manipulation and cleaning
+import plotly.express as px      #used for creating visual graphs
 print("hello world 4")
